be1af694de74ed7c32258b2d54b162a971591995
Noomaok <noomaok@gmail.com>
2021-01-19 08:40:26 +0100

    rearangement des pragmas

--- ExpManager.cpp
+++ ExpManager.cpp
@@ -364,50 +364,58 @@ void ExpManager::prepare_mutation(int indiv_id) const {
  *
  */
 void ExpManager::run_a_step() {
+    #pragma omp parallel
+    { 
+        // Running the simulation process for each organism
+        #pragma omp for schedule(static)
+        for (int indiv_id = 0; indiv_id < nb_indivs_; indiv_id++) {
+            selection(indiv_id);
+            prepare_mutation(indiv_id);
 
-    // Running the simulation process for each organism
-#pragma omp parallel for schedule(static)
-    for (int indiv_id = 0; indiv_id < nb_indivs_; indiv_id++) {
-        selection(indiv_id);
-        prepare_mutation(indiv_id);
+            if (dna_mutator_array_[indiv_id]->hasMutate()) {
+                auto &mutant = internal_organisms_[indiv_id];
+                mutant->apply_mutations(dna_mutator_array_[indiv_id]->mutation_list_);
+                mutant->evaluate(target);
+            }
+        }
 
-        if (dna_mutator_array_[indiv_id]->hasMutate()) {
-            auto &mutant = internal_organisms_[indiv_id];
-            mutant->apply_mutations(dna_mutator_array_[indiv_id]->mutation_list_);
-            mutant->evaluate(target);
+        // Swap Population
+        #pragma omp for schedule(static)
+        for (int indiv_id = 0; indiv_id < nb_indivs_; indiv_id++) {
+            prev_internal_organisms_[indiv_id] = internal_organisms_[indiv_id];
+            internal_organisms_[indiv_id] = nullptr;
+        }
+
+        // Search for the best
+        #pragma omp single
+        {
+            double best_fitness = prev_internal_organisms_[0]->fitness;
+            int idx_best = 0;
+            for (int indiv_id = 1; indiv_id < nb_indivs_; indiv_id++) {
+                if (prev_internal_organisms_[indiv_id]->fitness > best_fitness) {
+                    idx_best = indiv_id;
+                    best_fitness = prev_internal_organisms_[indiv_id]->fitness;
+                }
+            }
+            best_indiv = prev_internal_organisms_[idx_best];
+
+            // Stats
+            stats_best->reinit(AeTime::time());
+            stats_mean->reinit(AeTime::time());
+        }
+
+        #pragma omp for schedule(static)
+        for (int indiv_id = 0; indiv_id < nb_indivs_; indiv_id++) {
+            if (dna_mutator_array_[indiv_id]->hasMutate())
+                prev_internal_organisms_[indiv_id]->compute_protein_stats();
+        }
+
+        #pragma omp single
+        {
+            stats_best->write_best(best_indiv);
+            stats_mean->write_average(prev_internal_organisms_, nb_indivs_);
         }
     }
-
-    // Swap Population
-#pragma omp parallel for schedule(static)
-    for (int indiv_id = 0; indiv_id < nb_indivs_; indiv_id++) {
-        prev_internal_organisms_[indiv_id] = internal_organisms_[indiv_id];
-        internal_organisms_[indiv_id] = nullptr;
-    }
-
-    // Search for the best
-    double best_fitness = prev_internal_organisms_[0]->fitness;
-    int idx_best = 0;
-    for (int indiv_id = 1; indiv_id < nb_indivs_; indiv_id++) {
-        if (prev_internal_organisms_[indiv_id]->fitness > best_fitness) {
-            idx_best = indiv_id;
-            best_fitness = prev_internal_organisms_[indiv_id]->fitness;
-        }
-    }
-    best_indiv = prev_internal_organisms_[idx_best];
-
-    // Stats
-    stats_best->reinit(AeTime::time());
-    stats_mean->reinit(AeTime::time());
-
-#pragma omp parallel for schedule(static)
-    for (int indiv_id = 0; indiv_id < nb_indivs_; indiv_id++) {
-        if (dna_mutator_array_[indiv_id]->hasMutate())
-            prev_internal_organisms_[indiv_id]->compute_protein_stats();
-    }
-
-    stats_best->write_best(best_indiv);
-    stats_mean->write_average(prev_internal_organisms_, nb_indivs_);
 }
 
 void ExpManager::first_eval() {
